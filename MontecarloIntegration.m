4b76719bc5fac87dc557054d5e4a5efb89a3fd31
davidjadu <davidjadu@hotmail.com>
2016-02-02 22:25:03 -0500

    matlab file

---
+++ MontecarloIntegration.m
@@ -0,0 +1,12 @@
+e=10^-3;
+p=0.85;
+N=floor(9/((p)*(e^2)));
+counter = 0;
+for i =0:N
+    x=rand(3,1);
+    if (and(2*x(3)<=x(1)^4+x(2)^2 , x(1)^2+x(2)^2<=1))
+        counter = counter +1;
+    end
+end
+result=(counter*2)/N;
+disp(result);
